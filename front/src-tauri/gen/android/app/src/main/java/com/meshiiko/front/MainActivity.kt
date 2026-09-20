70fb92be906e34db3aee38b95449d145b9e6d3f1
Miyagoshi_Sota <k23122kk@aitech.ac.jp>
2026-09-20 16:06:29 +0900

    androidのビルドテストを完了

--- front/src-tauri/gen/android/app/src/main/java/com/meshiiko/front/MainActivity.kt
+++ front/src-tauri/gen/android/app/src/main/java/com/meshiiko/front/MainActivity.kt
@@ -2,7 +2,6 @@ package com.meshiiko.front
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import app.tauri.TauriActivity
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
