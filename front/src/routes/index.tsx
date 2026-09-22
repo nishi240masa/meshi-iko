f84690e1bd281cbdede40c4f40636c2f1090dacf
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-09-22 13:11:53 +0900

    add:tanstack routerを導入して既存実装を修正

---
+++ front/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { createFileRoute } from '@tanstack/react-router'
+import { Greet } from '../components/Greet'
+
+export const Route = createFileRoute('/')({
+  component: HomeComponent,
+})
+
+function HomeComponent() {
+  return (
+	<main>
+	  <h1>Welcome to Tauri + React</h1>
+	  <Greet />
+	</main>
+  )
+}
