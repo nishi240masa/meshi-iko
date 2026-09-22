86f9300f7874cea56f436faad4dec44e3dedf519
mizunoryuki <135314422+mizunoryuki@users.noreply.github.com>
2026-09-22 15:01:00 +0900

    fix:review

--- front/eslint.config.js
+++ front/eslint.config.js
@@ -3,7 +3,7 @@ import tseslint from "typescript-eslint";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
-import { reactRefresh } from "eslint-plugin-react-refresh";
+import reactRefresh from "eslint-plugin-react-refresh";
 import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 
@@ -65,7 +65,7 @@ export default defineConfig([
 
   // Vite の Fast Refresh 対応
   {
-    ...reactRefresh.configs.vite(),
+    ...reactRefresh.configs.vite,
     files: reactFiles,
   },
 
